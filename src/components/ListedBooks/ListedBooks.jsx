16dc59996c4e0ed5333b83fa99204bbd53cece98
tainur <tainurrahaman31@gmail.com>
2024-11-22 21:01:24 +0600

    book sort by pages and ratings

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -8,6 +8,7 @@ import ListedBook from "../ListedBook/ListedBook";
 const ListedBooks = () => {
   const allbooks = useLoaderData();
   const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
     const storeReadList = getReadStoreList();
@@ -18,6 +19,22 @@ const ListedBooks = () => {
     setReadList(readBookList);
   }, []);
 
+  const handleSort = (sortType) => {
+    setSort(sortType);
+
+    if (sortType === "pages") {
+      const sortReadList = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages
+      );
+      setReadList(sortReadList);
+    }
+
+    if (sortType === "ratings") {
+      const sortReadList = [...readList].sort((a, b) => a.rating - b.rating);
+      setReadList(sortReadList);
+    }
+  };
+
   return (
     <div>
       <Tabs>
@@ -26,13 +43,32 @@ const ListedBooks = () => {
           <Tab>Listed Books</Tab>
         </TabList>
 
+        <div className="flex justify-center">
+          <div className="dropdown dropdown-bottom ">
+            <div tabIndex={0} role="button" className="btn m-1">
+              {sort ? `Sort by ${sort}` : "Sort by"}
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li onClick={() => handleSort("ratings")}>
+                <a>Rating</a>
+              </li>
+              <li onClick={() => handleSort("pages")}>
+                <a>Pages</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <TabPanel>
           {readList.map((book) => (
             <ListedBook key={book.bookId} book={book}></ListedBook>
           ))}
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          <h2>Wish List</h2>
         </TabPanel>
       </Tabs>
     </div>
